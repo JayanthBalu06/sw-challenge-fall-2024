06175a333edcc07c509868d191e97a5aae61e3a1
Jayanth Balu <jaybalu06@gmail.com>
2024-09-25 19:45:05 -0500

    reworked pandas to use csv library instead. fixed threading aswell

--- ctg_deliverable.py
+++ ctg_deliverable.py
@@ -1,6 +1,20 @@
-import pandas as pd
+import csv
 import glob 
 from concurrent.futures import ThreadPoolExecutor
+import os
+
+
+def read_csv_file(file):
+    """Helper function to read a single CSV file."""
+    data = []
+    try:
+        with open(file, newline='') as f:
+            reader = csv.DictReader(f)
+            data.extend(list(reader))
+    except Exception as e:
+        print(f"Error reading {file}: {e}")
+    return data
+
 
 def loadData(path):
 	#specifiy directory of timetick data
@@ -10,43 +24,51 @@ def loadData(path):
 	csv_files = glob.glob(f'{data_directory}*.csv')
 
 
+	#initalize list to hold all tick data
+	all_data = []
+
 	#initialize threadpoolexecutor object
-	executor = ThreadPoolExecutor()
+	executor = ThreadPoolExecutor(max_workers = 8)
+
+	futures = []
+	for file in csv_files:
+		#load each csv processing task as a seperate future to be executed asyncrhonously 
+		future = executor.submit(read_csv_file,file)
+		futures.append(future)
+
+	#append each processed csv result to alldata
+	for future in futures:
+		try:
+			#result only gets populated if current future status is completed
+			result = future.result()
+			all_data.extend(result)
+		except Exception as e:
+			print(f"could not process file")
+
+	#abort threadpool after use
+	executor.shutdown()
+
+	return all_data
+
+	
 
 
-#handle exceptions
-	try:
-	#execute multple threads to read csv files
-		dfs = list(executor.map(pd.read_csv,csv_files))
-	except FileNotFoundError as e:
-	    print(f"Error: One of the files was not found. {e}")
-	except pd.errors.EmptyDataError as e:
-	    print(f"Error: One of the files is empty. {e}")
-	except pd.errors.ParserError as e:
-	    print(f"Error: There was a problem parsing a file. {e}")
-	except Exception as e:
-	    print(f"An unexpected error occurred: {e}")
-	finally:
-	#shutdown object
-		executor.shutdown()
 
-	#concatenate each data frame into one
-	df = pd.concat(dfs, ignore_index = True)
+def writeCsv(data, output_file_path):
 
-	return df
+	headers = data[0].keys();
+	with open(output_file_path, mode = "w", newline = '') as file:
+		writer = csv.DictWriter(file, fieldnames = headers)
+		writer.writeheader()
+		writer.writerows(data)
 
 
 def main():
-	df = loadData('data/')
+	#cpu_count = os.cpu_count()
+	#print(f"Number of CPUs: {cpu_count}")
 
-	df_filtered = df[(df['Price'] >= 0) & (df['Size'] >= 0)]
-	print(df_filtered.head())
-
-	# Check the data types of each column
-	print(df_filtered.dtypes)
-
-	# Get summary statistics
-	print(df_filtered.describe(include='all'))
-
+	allData = loadData('data/')
+	writeCsv(allData, 'tickdata.csv')
+	
 if __name__ == "__main__":
 	main()
